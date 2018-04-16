bbc9ba1d197e22c837b98a6aa54f4edfe911b322
Charles Munat <Charles_Munat@bnz.co.nz>
2018-04-17 11:16:46 +1200

    Add getMoves selector

--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,6 @@
 import { squareClicked } from './actions'
 import { SQUARE_CLICKED } from './constants'
 import { initialState, rootReducer } from './reducers'
+import { getMoves } from './selectors'
 
-export { initialState, rootReducer, SQUARE_CLICKED, squareClicked }
+export { getMoves, initialState, rootReducer, SQUARE_CLICKED, squareClicked }
